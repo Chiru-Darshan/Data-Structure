db9349b744694b2336a8ac01a81c77664de654f9
Darshan A <darshan.a@microfocus.com>
2021-08-21 15:59:55 +0530

    breadth first search implementation for graph

---
+++ Graphs/Bfs.js
@@ -0,0 +1,22 @@
+const graph = {
+    a: ['c', 'b'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+}
+
+const bfs_search = (graph, source) => {
+    let queue = [source]
+    while (queue.length > 0) {
+        const current = queue.shift()
+        console.log(current)
+        for (let neighbor of graph[current]) {
+            queue.push(neighbor)
+        }
+    }
+}
+
+
+bfs_search(graph, 'a')
